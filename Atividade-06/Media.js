a8df5b9b6d87ea73de74c827728c93c6043dec27
BrunaSCamargo <122629026+BrunaSCamargo@users.noreply.github.com>
2024-09-22 09:12:10 -0300

    Add files via upload

---
+++ Atividade-06/Media.js
@@ -0,0 +1,13 @@
+var nome = prompt("Digite o nome do aluno: ")
+
+var n1 = parseFloat(prompt("Digite a nota 1: "))
+var n2 = parseFloat(prompt("Digite a nota 2: "))
+var n3 = parseFloat(prompt("Digite a nota 3: "))
+var n4 = parseFloat(prompt("Digite a nota 4: "))
+
+
+
+soma = n1+n2+n3+n4
+media = soma/4
+
+alert("Media: " + media)
---
+++ Atividade-06/Operacoes.js
@@ -0,0 +1,17 @@
+var num1 = parseFloat(prompt("Digite um número: "))
+var num2 = parseFloat(prompt("Digite mais um número: "))
+
+soma = num1+num2
+sub = num1 - num2
+prod = num1*num2
+divs = num1/num2
+resto = num1%num2
+
+
+alert("Soma = " + soma)
+alert("Subtração = " + sub)
+alert("Produto = " + prod)
+alert("Divisão = "+ divs)
+alert("Resto = " + resto)
+
+

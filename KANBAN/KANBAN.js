be0e97c6509c4f92d6daa291d10f969750647fcc
BrunaSCamargo <122629026+BrunaSCamargo@users.noreply.github.com>
2024-11-18 22:32:32 -0300

    Update KANBAN.js

--- KANBAN/KANBAN.js
+++ KANBAN/KANBAN.js
@@ -1,18 +1,51 @@
 const $modal = document.getElementById('modal');
 const $descricaoInput = document.getElementById('descricao');
 const $prioridadeInput = document.getElementById('prioridade');
+const $idInput = document.getElementById("idInput");
+
+const $AdicionarTarefa = document.getElementById("AdicionarTarefa");
+const $EditarTarefa = document.getElementById('EditarTarefa');
+
+const $salvar = document.getElementById("salvar");
+const $salvarAlteracao = document.getElementById('salvarAlteracao');
+
 
 const $colPreparativosBody = document.querySelector('#colPreparativos .body');
 
 var listaTarefa = [];
 
-function openModal(){
+function openModal(id){
     $modal.style.display = "flex";
+
+    if (id) {
+        $AdicionarTarefa.style.display = "none";
+        $salvar.style.display = "none";
+
+        $EditarTarefa.style.display = "block";
+        $salvarAlteracao.style.display = "block";
+
+        const index = listaTarefa.findIndex(function(tarefa) {
+            return tarefa.id = id;
+        });
+
+        const task = listaTarefa[index];
+
+        $idInput.value = tarefa.id;
+        $descricaoInput.value = tarefa.descicao;
+        $prioridadeInput.value = tarefa.prioridade;
+    } else {
+        $AdicionarTarefa.style.display = "block";
+        $salvar.style.display = "block";
+
+        $EditarTarefa.style.display = "none";
+        $salvarAlteracao.style.display = "none";
+    }
 }
 
 function closeModal(){
     $modal.style.display = "none";
 
+    $idInput.value="";
     $descricaoInput.value="";
     $prioridadeInput.value="";
 
@@ -21,8 +54,9 @@ function closeModal(){
 function gerarCard() {
     const listaTarefaHTML = listaTarefa.map(function(tarefa){
         return `
-            <div class="card">
+            <div class="card" ondblclick="openModal(${tarefa.id})">
                 <div class="info">
+                    <b>Descrição:</b>
                     <span>${tarefa.descicao}</span>
                 </div>
 
@@ -39,6 +73,7 @@ function gerarCard() {
 
 function criarTarefa() {
     const novaTarefa = {
+        id: Math.floor(Math.random() * 999999),
         descicao: $descricaoInput.value,
         prioridade: $prioridadeInput.value,
     }
@@ -48,6 +83,17 @@ function criarTarefa() {
     closeModal();
 
     gerarCard();
+}
 
-    alert(listaTarefa);
+function atualizarTarefa() {
+    const newTarefa = {
+        id: $idInput.value,
+        descicao: $descricaoInput.value,
+        prioridade: $prioridadeInput.value,
+    }
+
+    const index = listaTarefa.findIndex(function(tarefa) {
+        return tarefa.id = id;
+    });
+
 }
